a93c01ceda08d8d5f34fbfdaf53d6ceb5a87e769
Irina Poloubessov <polubes@gmail.com>
2024-05-03 16:27:32 +0200

    distances matrix created, FastAPI started

---
+++ app/match.py
@@ -0,0 +1,14 @@
+# jupyter nbconvert --to script [YOUR_NOTEBOOK].ipynb
+
+
+from fastapi import FastAPI
+import sys
+sys.path.insert(0, '../')  # Adjust the path as necessary
+
+from nlp_medical_qa import print_results  # Import the desired function
+
+app = FastAPI()
+
+@app.post('/api/post_question')
+def post_question(question: int):
+    return print_results(question)
